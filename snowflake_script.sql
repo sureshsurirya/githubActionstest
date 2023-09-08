88450933808a27b8e010a802d6b6a3ea178af105
sureshsurirya <144013226+sureshsurirya@users.noreply.github.com>
2023-09-09 00:14:52 +0530

    Update snowflake_script.sql

--- snowflake_script.sql
+++ snowflake_script.sql
@@ -1,14 +1,57 @@
-CREATE OR REPLACE PROCEDURE CREATE_SAMPLE_TABLE()
-RETURNS STRING
-LANGUAGE SQL
-EXECUTE AS CALLER
+CREATE OR REPLACE PROCEDURE reader_from_actions()
+  RETURNS STRING
+  LANGUAGE SQL
+  EXECUTE AS CALLER
 AS
 $$
-BEGIN
-    -- Create a sample table named "my_sample_table"
-    CREATE OR REPLACE TABLE my_sample_table (id INT,name STRING,age INT);
+DECLARE
+  RESULT STRING;
+  SQL_STMT STRING;
+  LOCATOR STRING;
 
-    -- Return a success message
-    RETURN 'Sample table created successfully.';
+BEGIN
+  -- Create a managed account
+  SQL_STMT := 'CREATE MANAGED ACCOUNT MANAGED_ACCOUNT12445599 
+               ADMIN_NAME = ''ADMIN'' 
+               ADMIN_PASSWORD = ''Welcome@123'' 
+               TYPE = READER';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  -- Insert data into reader_details
+  SQL_STMT := 'INSERT INTO GH_ACTIONS_DB.GH_ACTIONS_SCM.reader_details 
+               (reader_account_details, cust_name, add_time_stamp) 
+               SELECT PARSE_JSON($1), ''YourCustomerName'', CURRENT_TIMESTAMP() 
+               FROM TABLE(result_scan(LAST_QUERY_ID()))';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  -- Fetch the first accountLocator
+  LOCATOR := (
+    SELECT reader_account_details:accountLocator
+    FROM GH_ACTIONS_DB.GH_ACTIONS_SCM.reader_details
+     order by add_time_stamp desc LIMIT 1
+  );
+
+  -- Create or replace a share
+  SQL_STMT := 'CREATE OR REPLACE SHARE CUST_SHARE';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  -- Grant USAGE on DATABASE
+  SQL_STMT := 'GRANT USAGE ON DATABASE GH_ACTIONS_DB TO SHARE CUST_SHARE';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  -- Grant USAGE on SCHEMA
+  SQL_STMT := 'GRANT USAGE ON SCHEMA GH_ACTIONS_DB.GH_ACTIONS_SCM TO SHARE CUST_SHARE';
+  EXECUTE IMMEDIATE SQL_STMT;
+  SQL_STMT := 'GRANT SELECT ON ALL TABLES IN SCHEMA GH_ACTIONS_DB.GH_ACTIONS_SCM TO SHARE CUST_SHARE';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  -- Alter the share and add the account without quotes
+  SQL_STMT := 'ALTER SHARE CUST_SHARE ADD ACCOUNT = ' || LOCATOR || '';
+  EXECUTE IMMEDIATE SQL_STMT;
+
+  RESULT := 'Reader Account, Share has been created, and reader account has been added to the Share';
+
+  RETURN RESULT;
 END;
 $$;
+call reader_from_actions();
