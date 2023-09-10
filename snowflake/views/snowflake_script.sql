a6429ba5b18130a64fa06a37f280fe23dbf47e63
sureshsurirya <144013226+sureshsurirya@users.noreply.github.com>
2023-09-11 02:04:22 +0530

    Update snowflake_script.sql

--- snowflake/views/snowflake_script.sql
+++ snowflake/views/snowflake_script.sql
@@ -10,17 +10,18 @@ DECLARE
   LOCATOR STRING;
   READER_NAME STRING;
   SHARE_NAME STRING;
+  CUST_NAME STRING;
 BEGIN
   READER_NAME := '&{CUST_NAME}' || '_READER';
   SHARE_NAME := '&{CUST_NAME}' || '_SHARE';
-
+  CUST_NAME := '&{CUST_NAME}'
   SQL_STMT := 'CREATE MANAGED ACCOUNT ' || READER_NAME || '
                ADMIN_NAME = ''ADMIN'' ADMIN_PASSWORD = ''Welcome@123'' TYPE = READER';
   EXECUTE IMMEDIATE SQL_STMT;
   
   SQL_STMT := 'INSERT INTO &{CUST_RAW_DB}.&{CUST_SCHEMA}.reader_details 
                (reader_account_details,cust_name,add_time_stamp) 
-               SELECT PARSE_JSON($1),${CUST_NAME}, CURRENT_TIMESTAMP() 
+               SELECT PARSE_JSON($1),&{CUST_NAME}, CURRENT_TIMESTAMP() 
                FROM TABLE(result_scan(LAST_QUERY_ID()))';
   EXECUTE IMMEDIATE SQL_STMT;
 
